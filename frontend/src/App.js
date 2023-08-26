0684b9860e437e58646ec9424f6c3827ab3d413f
Lilsofie <123223306+Lilsofie@users.noreply.github.com>
2023-08-26 01:17:18 -0400

    set Phase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
-import {useRef, useState}from 'react'
+import { useState}from 'react'
 import './App.css';
 import Start from './Pages/Start';
 import NameOptions from './Pages/Question';
 import Result from './Pages/Result';
-import { all } from 'axios';
 
 
 function App() {
-  const [pageNumber, setPageNumber] = useState(0);
-
+  const [PageNumber, setPageNumber] = useState(1);
+  const [Phase,setPhase] = useState(0);
   return <div>
         {
-          pageNumber === 0 ? 
+          PageNumber === 0 ? 
           <Start setPageFunc = {setPageNumber}/> : <></>
         }
         {
-          pageNumber === 1 ?
+          PageNumber === 1 ?
           <NameOptions setPageFunc = {setPageNumber}/> : <></>
         }
         {
-          pageNumber === 2 ?
-          <Result setPageFunc = {setPageNumber}/> : <></>
+          PageNumber === 2 ?
+          <Result setPageFunc = {setPageNumber} setPhaseFunc = {setPhase} Phase = {Phase}/> : <></>
         }
        
         
