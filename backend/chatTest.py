2156183f1ee664fd1233d9772e85ae5b51be4a6f
tjhiaj <j.tjhia@gmail.com>
2023-08-26 03:11:59 -0400

    Implementing text generation and updating engine

--- backend/chatTest.py
+++ backend/chatTest.py
@@ -1,10 +1,11 @@
 import openai
-from api_secrets import API_KEY #generate/find in dashboard and store in separate file
+
+from api_secrets import API_KEY
 
 openai.api_key = API_KEY
 
-prompt = "Say this is a test"
+prompt = "Generate a funny who-is-most-likely-to question that can be answered with a name."
 
-response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=6)
+response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=6)
 
 print(response)
